0ebee3493e07d81d4af6d4d2e0f97f83b085f07c
Jonathan Martin <me@nybblr.com>
2014-12-05 10:31:53 -0500

    Allow dispatcher to be specified.

--- lib/bnr/webhooks/controller.rb
+++ lib/bnr/webhooks/controller.rb
@@ -2,7 +2,10 @@ module Bnr
   module Webhooks
     module Controller
       def create
-        if Bnr::Webhooks::Receiver.process(request.raw_post, request.headers, worker: worker)
+        if Bnr::Webhooks::Receiver.process(request.raw_post,
+                                           request.headers,
+                                           worker: worker,
+                                           dispatcher: dispatcher)
           head status: :no_content
         else
           head status: :unprocessable_entity
@@ -13,6 +16,14 @@ module Bnr
 
       def worker
         Bnr::Webhooks::Worker
+      end
+
+      def dispatcher
+        Bnr::Webhooks::Dispatcher.new(mapping)
+      end
+
+      def mapping
+        {}
       end
     end
   end
